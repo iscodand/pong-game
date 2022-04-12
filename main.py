5f5ff46a41a23970a744123ac1595d7a2d42f31f
iscodandrade <iscodandrade@hotmail.com>
2022-04-12 20:03:54 -0300

    fixing bugs

--- main.py
+++ main.py
@@ -12,30 +12,30 @@ def start_menu():
                 quit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
+                    score_time
                     game()
                 elif event.key == pygame.K_q:
                     pygame.quit()
                     quit()
 
         screen.fill(pygame.Color('black'))
-        pong_game = start_font.render(f"PONG GAME", False, white)
-        pong_game_rect = pong_game.get_rect(center=(screen_width/2, 350))
+        
+        pong_game = large_font.render(f"PONG GAME", False, white)
+        pong_game_rect = pong_game.get_rect(center=(screen_width/2, 300))
         screen.blit(pong_game, pong_game_rect)
 
-        start = game_font.render(f"PRESS SPACE TO START", False, white)
+        start = small_font.render(f"PRESS SPACE TO START", False, white)
         start_rect = start.get_rect(center=(screen_width/2, 400))
         screen.blit(start, start_rect)
 
-        license_isco = credits_font.render(
+        license_isco = verysmall_font.render(
             f"Powered by Isco D'Andrade", False, white)
         license_isco_rect = license_isco.get_rect(center=(screen_width/2, 650))
         screen.blit(license_isco, license_isco_rect)
 
-
-
-
         pygame.display.update()
         clock.tick(60)
+
 
 def pause():
     paused = True
@@ -53,26 +53,26 @@ def pause():
 
         screen.fill(pygame.Color('black'))
 
-        pong_game = game_font.render(f"PONG GAME", False, white)
+        pong_game = small_font.render(f"PONG GAME", False, white)
         pong_game_rect = pong_game.get_rect(center=(screen_width/2, 50))
         screen.blit(pong_game, pong_game_rect)
 
-        pause_text = pause_font.render(f"GAME PAUSED", False, white)
+        pause_text = big_font.render(f"GAME PAUSED", False, white)
         pause_text_rect = pause_text.get_rect(center=(screen_width/2, 250))
         screen.blit(pause_text, pause_text_rect)
 
-        continue_text = game_font.render(
+        continue_text = medium_font.render(
             f"PRESS C TO 'CONTINUE'", False, white)
         continue_text_rect = continue_text.get_rect(
             center=(screen_width/2, screen_height/2 + 10))
         screen.blit(continue_text, continue_text_rect)
 
-        quit_text = game_font.render(f"PRESS Q TO 'QUIT'", False, white)
+        quit_text = medium_font.render(f"PRESS Q TO 'QUIT'", False, white)
         quit_text_rect = quit_text.get_rect(
             center=(screen_width/2, screen_height/2 + 50))
         screen.blit(quit_text, quit_text_rect)
 
-        license_isco = credits_font.render(
+        license_isco = verysmall_font.render(
             f"Powered by Isco D'Andrade", False, white)
         license_isco_rect = license_isco.get_rect(center=(screen_width/2, 650))
         screen.blit(license_isco, license_isco_rect)
@@ -152,15 +152,15 @@ def ball_start():
     ball.center = (screen_width/2, screen_height/2)
 
     if current_time - score_time < 700:
-        number_three = game_font.render("3", False, white)
+        number_three = medium_font.render("3", False, white)
         screen.blit(number_three, (screen_width/2 - 5, 70))
 
     if 700 < current_time - score_time < 1400:
-        number_two = game_font.render("2", False, white)
+        number_two = medium_font.render("2", False, white)
         screen.blit(number_two, (screen_width/2 - 5, 70))
 
     if 1400 < current_time - score_time < 2100:
-        number_one = game_font.render("1", False, white)
+        number_one = medium_font.render("1", False, white)
         screen.blit(number_one, (screen_width/2 - 5, 70))
 
     if current_time - score_time < 2100:
@@ -171,6 +171,7 @@ def ball_start():
         ball_speed_x = 11 * random.choice((1, -1))
         score_time = None
 
+
 # Starting General Setup
 pygame.init()
 clock = pygame.time.Clock()
@@ -179,14 +180,19 @@ clock = pygame.time.Clock()
 screen_width = 1300
 screen_height = 700
 screen = pygame.display.set_mode((screen_width, screen_height))
+screen_rect = screen.get_rect()
 pygame.display.set_caption('Pong Game')
 
 # Fonts
-credits_font = pygame.font.SysFont('Network', 15)
-player_font = pygame.font.SysFont('Network', 22)
-game_font = pygame.font.SysFont('Network', 30)
-pause_font = pygame.font.SysFont('Network', 48)
-start_font = pygame.font.SysFont('Network', 60)
+verysmall_font = pygame.font.SysFont('Network', 15)
+small_font = pygame.font.SysFont('Network', 22)
+medium_font = pygame.font.SysFont('Network', 30)
+big_font = pygame.font.SysFont('Network', 48)
+large_font = pygame.font.SysFont('Network', 90)
+
+# Blink Event
+blink_event = pygame.USEREVENT + 0
+empty = (0, 0, 0)
 
 # Defining Objects (Rectangles)
 ball = pygame.Rect(screen_width/2 - 15, screen_height/2 - 15, 20, 20)
@@ -210,11 +216,12 @@ player_score = 0
 opponent_score = 0
 
 # Defining score time
-score_time = True
+score_time = False
 
 # Sounds
 pong_sound = pygame.mixer.Sound("Sounds\pong.wav")
 score_sound = pygame.mixer.Sound("Sounds\score.wav")
+
 
 def game():
     global player_speed, opponent_speed
@@ -264,21 +271,21 @@ def game():
         pygame.draw.rect(screen, white, opponent)
         pygame.draw.ellipse(screen, white, ball)
         pygame.draw.aaline(screen, white, (screen_width/2, 100),
-                        (screen_width/2, screen_height))
+                           (screen_width/2, screen_height))
         pygame.draw.circle(screen, white,
-                        (screen_width/2, screen_height/2), 40, 1)
+                           (screen_width/2, screen_height/2), 40, 1)
 
         if score_time:
             ball_start()
 
         # Defining player and opponent points
-        player_text = game_font.render(f"{player_score}", False, white)
+        player_text = big_font.render(f"{player_score}", False, white)
         screen.blit(player_text, (695, 30))
 
-        equal = game_font.render(f":", False, white)
+        equal = big_font.render(f":", False, white)
         screen.blit(equal, (screen_width/2 - 5, 30))
 
-        opponent_text = game_font.render(f"{opponent_score}", False, white)
+        opponent_text = big_font.render(f"{opponent_score}", False, white)
         screen.blit(opponent_text, (590, 30))
 
         # Defining FPS
